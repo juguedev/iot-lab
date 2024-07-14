e223e21f1c18dc94ebd59b56ab47dfe6e9a2646d
juguedev <juguer.anra@gmail.com>
2024-07-14 11:12:14 -0500

    Distribute code in different functions

--- espressif/esp32/obdTesting/prueba_obd.ino
+++ espressif/esp32/obdTesting/prueba_obd.ino
@@ -3,74 +3,66 @@
 
 
 BluetoothSerial SerialBT;
-#define ELM_PORT   SerialBT
-#define DEBUG_PORT Serial
+#define SerialELM SerialBT
+#define SerialMon Serial
 
 char protocol = ISO_15765_11_BIT_500_KBAUD;
 
 //String MACadd = "DC:0D:30:48:8D:60";  00:10:cc:4f:36:03    //enter the ELM327 MAC address
-uint8_t address[6]  = {0x00, 0x10, 0xCC, 0x4F, 0x36, 0x03};  //enter the ELM327 MAC address after the 0x
+uint8_t address[6] = { 0x00, 0x10, 0xCC, 0x4F, 0x36, 0x03 };  //enter the ELM327 MAC address after the 0x
 
 
 ELM327 myELM327;
 
-uint32_t rpm = 0;
+
+void connectToBluetoothDevice() {
+  SerialMon.println("Connecting to ELM327 - Phase 1: Bluetooth");
+
+  if (!SerialELM.connect(address)) {  // Bu default #define READY_TIMEOUT (10 * 1000)
+    SerialMon.println("Couldn't connect to ELM327 - Phase 1: Bluetooth");
+    SerialMon.println("Retryng...");
+    delay(2000);
+    connectToBluetoothDevice();
+  } else {
+    SerialMon.println("Connected to ELM327 - Phase 1: Bluetooth");
+  }
+}
+
+void establishOBDConnection() {
+  SerialMon.println("Connecting to ELM327 - Phase 2: OBD");
+  if (!myELM327.begin(SerialELM, protocol = 'A')) {
+    SerialMon.println("Couldn't connect to OBD scanner - Phase 2: OBD");
+    SerialMon.println("Retryng...");
+    delay(2000);
+    establishOBDConnection();
+  } else {
+    SerialMon.println("Connected to ELM327 - Phase 2: OBD");
+  }
+
+  SerialMon.println("Connection process to ELM327 succesfully ended.");
 
 
-void setup()
-{
-#if LED_BUILTIN
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-#endif
-
-  DEBUG_PORT.begin(38400);
+}
+void setup() {
+  SerialMon.begin(115200);
+  SerialELM.begin("ESP32", true);
   //SerialBT.setPin("1234");
-  ELM_PORT.begin("ESP32", true);
 
-  if (!ELM_PORT.connect(address))            //"OBDII" replaced by "address"
-  {
-    DEBUG_PORT.println("Couldn't connect to OBD scanner - Phase 1");
-    while (1);
-  }
-
-  if (!myELM327.begin(ELM_PORT, protocol='A'))
-  {
-    Serial.println("Couldn't connect to OBD scanner - Phase 2");
-    while (1);
-  }
-
-  Serial.println("Connected to ELM327");
+  connectToBluetoothDevice();
+  establishOBDConnection();
 }
 
 
-void loop()
-{
-  float tempRPM = myELM327.rpm();
+void loop() {
+  float tempRPM = (uint32_t)myELM327.rpm();
 
-  if (myELM327.nb_rx_state == ELM_SUCCESS)
-  {
-    rpm = (uint32_t)tempRPM;
-    Serial.print("RPM: "); Serial.println(rpm);
+  if (myELM327.nb_rx_state == ELM_SUCCESS) {
+    Serial.print("RPM: "); Serial.println(tempRPM);
+  } else {
+    Serial.print("ERROR: ");
+    for (byte i = 0; i < myELM327.recBytes; i++)
+      Serial.write(myELM327.payload[i]);
+    Serial.println();
   }
-  else
-  {
-    printError();
-  }
-}
-
-
-void printError()
-{
-  Serial.print("Received: ");
-  for (byte i = 0; i < myELM327.recBytes; i++)
-    Serial.write(myELM327.payload[i]);
-  Serial.println();
-
-  if (myELM327.nb_rx_state == ELM_SUCCESS)
-    Serial.println(F("\tELM_SUCCESS"));
-  else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
-    myELM327.printError();
-
-  delay(100);
+  delay(1000);
 }
